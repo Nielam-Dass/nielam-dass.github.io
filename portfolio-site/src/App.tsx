68df4e977101f1901561779f53f91be17ce7eba5
Nielam Dass <nielam.dass@gmail.com>
2025-08-27 01:19:36 -0400

    Set up HashRouter for SPA navigation between site pages

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 style={{textAlign: "center"}}>Dev portfolio site is under construction</h1>
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>}/>
+      <Route path="/projects" element={<h1>Projects Page</h1>}/>
+      <Route path="/skills" element={<h1>Skills Page</h1>}/>
+      <Route path="/contact" element={<h1>Contact Page</h1>}/>
+    </Routes>
     </>
   );
 }
 
-export default App
+export default App;
