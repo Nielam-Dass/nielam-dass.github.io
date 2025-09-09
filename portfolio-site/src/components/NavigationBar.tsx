95b1c4fe43e6993b78de9466edd9309108021bc2
Nielam Dass <nielam.dass@gmail.com>
2025-09-08 21:05:17 -0400

    Added some basic initial styling to navigation bar with Tailwind

--- portfolio-site/src/components/NavigationBar.tsx
+++ portfolio-site/src/components/NavigationBar.tsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 function NavigationBar() {
     return (
         <>
-        <div style={{display: "flex", gap: "8px"}}>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/projects"}>Projects</Link>
-            <Link to={"/skills"}>Skills</Link>
-            <Link to={"/contact"}>Contact</Link>
-        </div>
+        <nav className="bg-black text-white flex justify-between p-3">
+            <div className="font-bold">NIELAM DASS</div>
+            <div className="flex gap-4">
+                <Link to={"/"}>Home</Link>
+                <Link to={"/projects"}>Projects</Link>
+                <Link to={"/skills"}>Skills</Link>
+                <Link to={"/contact"}>Contact</Link>
+            </div>
+        </nav>
         </>
     );
 }
