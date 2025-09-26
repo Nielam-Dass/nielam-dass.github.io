38d62b888cd983a915c5bcea218eefdd06b909bd
Nielam Dass <nielam.dass@gmail.com>
2025-09-26 00:36:13 -0400

    Added the About Me section to HomePage component

--- portfolio-site/src/pages/home/HomePage.tsx
+++ portfolio-site/src/pages/home/HomePage.tsx
@@ -1,9 +1,18 @@
 import type { JSX } from "react";
+import selfieImage from "../../assets/selfie_image.png";
 
 
 function HomePage(): JSX.Element {
     return (
-        <h1 className="text-2xl font-bold">Home Page</h1>
+        <>
+        <div id="about-section" className="flex gap-[150px] px-[140px] pt-[100px] pb-[30px]">
+            <div>
+                <h2 className="text-[36px] font-bold">About me</h2>
+                <p className="text-[24px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet recusandae at, veritatis illum eaque laborum perspiciatis mollitia iste sed expedita accusamus veniam exercitationem, reiciendis voluptatibus quaerat harum ullam accusantium et!</p>
+            </div>
+            <img src={selfieImage} alt="Selfie" className="w-[224px] h-[224px] min-w-[224px] rounded-full"/>
+        </div>
+        </>
     );
 }
 
