ae63a358c8b5d36bf2218f44210a95da05e870f5
Nielam Dass <nielam.dass@gmail.com>
2025-10-31 00:54:03 -0400

    Added personal contact details through JSON data file

--- portfolio-site/src/pages/contact/ContactInfoList.tsx
+++ portfolio-site/src/pages/contact/ContactInfoList.tsx
@@ -1,24 +1,18 @@
 import type { JSX } from "react";
 import ContactInfoCard from "./ContactInfoCard";
 
+import contactInfo from "../../data/contactInfo.json";
+
 
 function ContactInfoList(): JSX.Element {
     return (
         <>
         <div className="flex flex-col">
-            <ContactInfoCard
-                contactMethod="Contact Method 1"
-                contactLink="Link 1"
-                extraMessage="Extra message 1"
-                contactIconSrc="x"
-            />
-            <br />
-            <ContactInfoCard
-                contactMethod="Contact Method 2"
-                contactLink="Link 2"
-                extraMessage="Extra message 2"
-                contactIconSrc="x"
-            />
+            {contactInfo.map((contactOption, index): JSX.Element => {
+                return (
+                    <ContactInfoCard {...contactOption} key={index}/>
+                );
+            })}
         </div>
         </>
     );
