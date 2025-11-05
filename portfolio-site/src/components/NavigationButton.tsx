fe60e1212cbe5bf8d2dc5818f948eb2184ad64f3
Nielam Dass <nielam.dass@gmail.com>
2025-11-05 00:40:29 -0500

    Added hover and click effects to NavigationButton component

--- portfolio-site/src/components/NavigationButton.tsx
+++ portfolio-site/src/components/NavigationButton.tsx
@@ -9,7 +9,7 @@ interface NavigationButtonProps {
 
 function NavigationButton({linkTo, children}: NavigationButtonProps): JSX.Element {
     return (
-        <Link to={linkTo} className="flex items-center px-[30px] text-[36px] font-light">{children}</Link>
+        <Link to={linkTo} className="flex items-center px-[30px] text-[36px] font-light hover:bg-black duration-300 ease-out active:bg-white active:text-black active:duration-150">{children}</Link>
     );
 }
 
