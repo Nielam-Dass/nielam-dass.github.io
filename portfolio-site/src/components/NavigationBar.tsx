23de4f2cae5ae97f5c1393912135e7cead805c75
Nielam Dass <nielam.dass@gmail.com>
2025-11-07 10:56:10 -0500

    Made NavigationBar position sticky

--- portfolio-site/src/components/NavigationBar.tsx
+++ portfolio-site/src/components/NavigationBar.tsx
@@ -5,7 +5,7 @@ import NavigationButton from "./NavigationButton";
 function NavigationBar(): JSX.Element {
     return (
         <>
-        <nav className="bg-[#272727] text-white flex justify-between h-[128px]">
+        <nav className="bg-[#272727] text-white flex justify-between h-[128px] sticky top-0 z-10">
             <div className="flex items-center text-[40px] font-bold p-[40px]">NIELAM DASS</div>
             <div className="flex">
                 <NavigationButton linkTo="/">HOME</NavigationButton>
