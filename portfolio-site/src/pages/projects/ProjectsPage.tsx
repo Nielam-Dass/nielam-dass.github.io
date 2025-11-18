575aa25fb2ff1546fce2414e7827de6750f551d4
Nielam Dass <nielam.dass@gmail.com>
2025-11-18 02:06:45 -0500

    Moved project list into a separate JSON data file

--- portfolio-site/src/pages/projects/ProjectsPage.tsx
+++ portfolio-site/src/pages/projects/ProjectsPage.tsx
@@ -1,53 +1,14 @@
 import type { JSX } from "react";
 import ProjectSearchBar from "./ProjectSearchBar";
 import ProjectSearchResults from "./ProjectSearchResults";
+import projectList from "../../data/projectList.json";
 
 
 function ProjectsPage(): JSX.Element {
-    const exampleProjectList = [
-        {
-            projectName: "Project 1",
-            projectDescription: [
-                "Description of project 1",
-                "More description of project 1",
-                "Even more description of project 1"
-            ],
-            skillsUsed: ["ABC", "DEF"],
-            projectLinks: [
-                {linkText: "ExampleCom", linkDest: "https://example.com/"}, 
-                {linkText: "ExampleNet", linkDest: "https://example.net/"}
-            ]
-        },
-        {
-            projectName: "Project 2",
-            projectDescription: [
-                "Description of project 2",
-                "More description of project 2"
-            ],
-            skillsUsed: ["GHI", "JKL", "DEF"]
-        },
-        {
-            projectName: "Project 3",
-            projectDescription: [
-                "Description of project 3"
-            ],
-            skillsUsed: ["JKL", "ABC"],
-            projectLinks: [
-                {linkText: "ExampleCom", linkDest: "https://example.com/"}
-            ]
-        },
-        {
-            projectName: "Project 4",
-            projectDescription: [
-                "Description of project 4"
-            ],
-            skillsUsed: ["DEF", "MNO"]
-        }
-    ];
     return (
         <div className="flex flex-col gap-[50px] pb-[20px]">
             <ProjectSearchBar/>
-            <ProjectSearchResults projectList={exampleProjectList}/>
+            <ProjectSearchResults projectList={projectList}/>
         </div>
     );
 }
