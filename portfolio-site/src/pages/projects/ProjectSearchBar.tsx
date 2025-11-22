7c60b15743ae34ef1da037c7f520b0c7d6a04669
Nielam Dass <nielam.dass@gmail.com>
2025-11-21 20:20:05 -0500

    Update project search results based on what user types into search bar

--- portfolio-site/src/pages/projects/ProjectSearchBar.tsx
+++ portfolio-site/src/pages/projects/ProjectSearchBar.tsx
@@ -1,11 +1,16 @@
+import type React from "react";
 import type { JSX } from "react";
 
 
-function ProjectSearchBar(): JSX.Element {
+interface ProjectSearchBarProps {
+    onSearchUpdate: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+function ProjectSearchBar(props: ProjectSearchBarProps): JSX.Element {
     return (
         <div className="flex flex-col items-center gap-[20px] pb-[40px] pt-[60px]">
             <h1 className="text-[48px] font-bold">BROWSE PROJECTS</h1>
-            <input type="text" placeholder="Search project name, description, or skills used" className="border-1 rounded-full w-[650px] px-[40px] py-[15px] text-[24px] font-light" id="project-search-input"/>
+            <input type="text" placeholder="Search project name, description, or skills used" onChange={props.onSearchUpdate} className="border-1 rounded-full w-[650px] px-[40px] py-[15px] text-[24px] font-light" id="project-search-input"/>
         </div>
     );
 }
--- portfolio-site/src/pages/projects/ProjectsPage.tsx
+++ portfolio-site/src/pages/projects/ProjectsPage.tsx
@@ -1,13 +1,29 @@
-import type { JSX } from "react";
+import React, { useState, type JSX } from "react";
 import ProjectSearchBar from "./ProjectSearchBar";
 import ProjectSearchResults from "./ProjectSearchResults";
-import projectList from "../../data/projectList.json";
+import fullProjectList from "../../data/projectList.json";
+import type { Project } from "./ProjectCard";
 
 
 function ProjectsPage(): JSX.Element {
+    const [projectList, setProjectList] = useState<Project[]>(fullProjectList);
+
+    const isRelevantToSearch = (content: string, search: string): boolean => content.toLowerCase().includes(search);
+
+    const handleSearchUpdate: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchQuery: string = event.target.value;
+        setProjectList((): Project[] => {
+            return fullProjectList.filter((project: Project) => {
+                return isRelevantToSearch(project.projectName, searchQuery) || 
+                    project.projectDescription.some((descriptionPoint: string) => isRelevantToSearch(descriptionPoint, searchQuery)) || 
+                    project.skillsUsed.some((skill: string) => isRelevantToSearch(skill, searchQuery));
+            });
+        });
+    }
+    
     return (
         <div className="flex flex-col gap-[50px] pb-[20px]">
-            <ProjectSearchBar/>
+            <ProjectSearchBar onSearchUpdate={handleSearchUpdate}/>
             <ProjectSearchResults projectList={projectList}/>
         </div>
     );
