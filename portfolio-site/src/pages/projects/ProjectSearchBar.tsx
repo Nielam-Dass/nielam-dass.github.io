aaccb2aeb774fc323719097add24043a22c40d29
Nielam Dass <nielam.dass@gmail.com>
2025-11-28 15:16:56 -0500

    Set input field type for project search bar as search and disabled autocomplete

--- portfolio-site/src/pages/projects/ProjectSearchBar.tsx
+++ portfolio-site/src/pages/projects/ProjectSearchBar.tsx
@@ -10,7 +10,7 @@ function ProjectSearchBar(props: ProjectSearchBarProps): JSX.Element {
     return (
         <div className="flex flex-col items-center gap-[20px] pb-[40px] pt-[60px]">
             <h1 className="text-[48px] font-bold">BROWSE PROJECTS</h1>
-            <input type="text" placeholder="Search project name, description, or skills used" onChange={props.onSearchUpdate} className="border-1 rounded-full w-[650px] px-[40px] py-[15px] text-[24px] font-light" id="project-search-input"/>
+            <input type="search" autoComplete="off" placeholder="Search project name, description, or skills used" onChange={props.onSearchUpdate} className="border-1 rounded-full w-[650px] px-[40px] py-[15px] text-[24px] font-light" id="project-search-input"/>
         </div>
     );
 }
