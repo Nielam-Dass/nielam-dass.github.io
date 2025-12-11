cdba56a557f84933d9321fe3a5fbe7422ad314bd
Nielam Dass <nielam.dass@gmail.com>
2025-12-11 01:42:25 -0500

    Move skills list data into separate JSON data file

--- portfolio-site/src/pages/skills/SkillsPage.tsx
+++ portfolio-site/src/pages/skills/SkillsPage.tsx
@@ -1,6 +1,7 @@
 import type { JSX } from "react";
 import SkillSearchBar from "./SkillSearchBar";
 import SkillsSearchResults from "./SkillsSearchResults";
+import skillList from "../../data/skillList.json";
 
 
 function SkillsPage(): JSX.Element {
@@ -8,36 +9,7 @@ function SkillsPage(): JSX.Element {
         <div className="flex flex-col gap-[40px] px-[140px] pb-[20px]">
             <SkillSearchBar/>
             <SkillsSearchResults 
-                skillList={[
-                    {
-                        skillName: "Skill 1",
-                        skillWeight: 10
-                    }, 
-                    {
-                        skillName: "Skill 2",
-                        skillWeight: 10
-                    }, 
-                    {
-                        skillName: "Skill 3",
-                        skillWeight: 8
-                    }, 
-                    {
-                        skillName: "Skill 4",
-                        skillWeight: 7
-                    }, 
-                    {
-                        skillName: "Skill 5"
-                    }, 
-                    {
-                        skillName: "Skill 6"
-                    }, 
-                    {
-                        skillName: "Skill 7"
-                    }, 
-                    {
-                        skillName: "Skill 8"
-                    }
-                ]}
+                skillList={skillList}
             />
         </div>
     );
