daeca8513bfd49eb7bf3dac12da1199f02178b4b
Nielam Dass <nielam.dass@gmail.com>
2025-12-23 01:29:32 -0500

    Display message in SkillsSearchResults component when no matching skills are found

--- portfolio-site/src/pages/skills/SkillsSearchResults.tsx
+++ portfolio-site/src/pages/skills/SkillsSearchResults.tsx
@@ -8,6 +8,11 @@ interface SkillsSearchResultsProps {
 }
 
 function SkillsSearchResults(props: SkillsSearchResultsProps): JSX.Element {
+  if(props.skillList.length===0) {
+    return (
+      <div className="flex justify-center text-[24px] font-bold">No matching skills found, but I'm always excited to learn something new!</div>
+    );
+  }
   return (
     <div className="grid grid-cols-3 gap-y-[20px] px-[40px] justify-items-center">
         {props.skillList.map((skill, index): JSX.Element => <SkillCard key={index} skill={skill}/>)}
