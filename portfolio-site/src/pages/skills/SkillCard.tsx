636baf799af0091012ede65c41c910a38b5b8bb5
Nielam Dass <nielam.dass@gmail.com>
2025-12-29 01:49:54 -0500

    Added CSS fallback fonts

--- portfolio-site/src/pages/skills/SkillCard.tsx
+++ portfolio-site/src/pages/skills/SkillCard.tsx
@@ -7,7 +7,7 @@ interface SkillCardProps {
 
 function SkillCard(props: SkillCardProps): JSX.Element {
   return (
-    <div className="flex justify-center text-[32px] font-[Courier_Prime] w-[320px] border-2 rounded-[10px] px-[40px] py-[15px]">
+    <div className="flex justify-center text-[32px] font-[Courier_Prime,monospace] w-[320px] border-2 rounded-[10px] px-[40px] py-[15px]">
         {props.skill.skillName}
     </div>
   );
